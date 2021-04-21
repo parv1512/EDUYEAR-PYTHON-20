298bfa9d952e0eb46fe677352e2ed21ec3b08bc4
parv1512 <82957269+parv1512@users.noreply.github.com>
2021-04-21 22:31:18 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,22 @@
+# part - 1 [Take variables with values of different types]
+Topic = 'Health'
+Name = "Parva Patel"
+Age = 14
+Weight = 50.35 
+Overweight = False
+
+# part - 2 [Print these in different lines and with appropriate messages (use .format()]
+print("Today's topic is {}".format(Topic))
+print("My name is {}.".format(Name), end=" ")
+print("I am {} years old.".format(Age))
+print("My weight is {}kg.".format(Weight), end=" ")
+print("I am overweight : {}".format((Overweight)))
+
+# part - 3 [Real world example of variable apart from which i gave in class]
+""" 
+Variables are numbers that don't have a fixed value, 
+examples : 
+1. Amount of money in our bank account, the amount keeps on changinh as we withdraw and deposit money. (eg. money=98500)
+2. Temperature of a particular place keeps on changing time to time it is not constant. (eg. temp=30°)
+3. The age of a person also increases year by year. (eg. age=14)
+"""
